8bb770b0a056857b49cf0f3d8ba4d0c00f39716a
Avi Goldman <avrahamymgoldman@gmail.com>
2018-09-11 09:39:06 -0700

    added test page for static query loader

---
+++ examples/kitchen-sink/src/pages/static-mdx-query.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+import MDXRenderer from "gatsby-mdx/mdx-renderer";
+
+export default function anotherPage() {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          mdx(id: { eq: "c9610a63-e336-5fdc-b2ca-abe0e4143e22" }) {
+            code {
+              scope
+              body
+            }
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <div>
+            <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
+          </div>
+        );
+      }}
+    />
+  );
+}
