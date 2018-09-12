38d1acf6d0565871baadbf33695f685f3334340d
Avi Goldman <avrahamymgoldman@gmail.com>
2018-09-12 10:23:38 -0700

    page query - use MDXScopeProvider to pass down the scope instead of injecting it into the MDXRenderer

--- packages/gatsby-mdx/loaders/page-graphql-mdx-loader.js
+++ packages/gatsby-mdx/loaders/page-graphql-mdx-loader.js
@@ -1,5 +1,5 @@
 /**
- * Loader for making MDX queried via StaticQuery work.
+ * Loader for making MDX queried via pageQuery work.
  *
  * For the next step in rendering from MDX node, see mdx-renderer.js
  */
@@ -11,9 +11,6 @@ const { babelParseToAst } = require("../utils/babel-parse-to-ast");
 const findScopes = require("../utils/find-scopes");
 const findPageQuery = require("../utils/find-page-query");
 const { WRAPPER_START } = require("../constants");
-
-const injectScopeIntoMDXRenderer = (code, scope) =>
-  code.replace(/<MDXRenderer/g, `$& scopes={${scope}}`);
 
 module.exports = async function(content) {
   const callback = this.async();
@@ -55,13 +52,19 @@ module.exports = async function(content) {
     .map(({ scopeId, scope }) => `import ${scopeId} from "${scope}";`)
     .join("\n");
 
-  const singleScopeObject = `{${scopes
-    .map(({ scopeId }) => scopeId)
-    .join(", ")}}`;
+  const mdxScopes = `{${scopes.map(({ scopeId }) => scopeId).join(", ")}}`;
 
-  const code = `${scopesImports}
+  const code = `import React from "react";
+import { MDXScopeProvider } from "gatsby-mdx/context";
+${scopesImports}
 
-${injectScopeIntoMDXRenderer(content, singleScopeObject)}`;
+import OriginalWrapper from "${originalFile}";
+
+export default ({children, ...props}) => <MDXScopeProvider scopes={${mdxScopes}}>
+  <OriginalWrapper {...props}>
+    {children}
+  </OriginalWrapper>
+</MDXScopeProvider>`;
 
   return callback(null, code);
 };
