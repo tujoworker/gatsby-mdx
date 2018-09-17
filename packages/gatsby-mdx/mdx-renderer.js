8cc31d9c8b1f0c7ee4cfe132944b6536faa300c0
Avi Goldman <avrahamymgoldman@gmail.com>
2018-09-16 20:58:30 -0700

    added ability to pass in additional scope to MDXRenderer

--- packages/gatsby-mdx/mdx-renderer.js
+++ packages/gatsby-mdx/mdx-renderer.js
@@ -2,20 +2,26 @@ import { withMDXComponents } from "@mdx-js/tag/dist/mdx-provider";
 import { withMDXScope } from "./context";
 
 export default withMDXScope(
-  withMDXComponents(({ scopes = {}, components = {}, children, ...props }) => {
-    const { scope, body } = children;
-    const fullScope = {
-      components,
-      props,
-      ...scopes[scope.id]
-    };
+  withMDXComponents(
+    ({ scopes = {}, scope = {}, components = {}, children, ...props }) => {
+      const {
+        scope: { id: scopeId },
+        body
+      } = children;
+      const fullScope = {
+        components,
+        props,
+        ...scope,
+        ...scopes[scopeId]
+      };
 
-    // body is pre-compiled mdx
-    const keys = Object.keys(fullScope);
-    const values = keys.map(key => fullScope[key]);
-    const fn = new Function("_fn", ...keys, `${body}`);
+      // body is pre-compiled mdx
+      const keys = Object.keys(fullScope);
+      const values = keys.map(key => fullScope[key]);
+      const fn = new Function("_fn", ...keys, `${body}`);
 
-    const end = fn({}, ...values)({ components, ...props });
-    return end;
-  })
+      const end = fn({}, ...values)({ components, ...props });
+      return end;
+    }
+  )
 );
